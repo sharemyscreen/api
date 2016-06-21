13fb9a5861d375bf55f62847f30497255fb85321
Maxime Guedj <max.guedj13@gmail.com>
2016-06-21 18:44:51 +0200

    Add dev service bindings !

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
+const vhost = require('vhost');
+const config = require('config');
+const devService = require('dev-service');
 
 function registerApp (masterApp) {
-
+  masterApp.use(vhost(config.get('devService.url'), devService.getApp()));
 }
 
 module.exports.registerApp = registerApp;
