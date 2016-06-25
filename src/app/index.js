ffbf2c20318ca387d651f0c119ea5b81585de854
Maxime Guedj <max.guedj13@gmail.com>
2016-06-25 15:54:42 +0200

    Integrate api service

--- src/app/index.js
+++ src/app/index.js
@@ -2,10 +2,12 @@ const vhost = require('vhost');
 const config = require('config');
 const devService = require('sharemyscreen-dev-service');
 const loginService = require('sharemyscreen-login-service');
+const apiService = require('sharemyscreen-api-service');
 
 function registerApp (masterApp) {
   masterApp.use(vhost(config.get('devService.url'), devService.getApp()));
   masterApp.use(vhost(config.get('loginService.url'), loginService.getApp()));
+  masterApp.use(vhost(config.get('apiService.url'), apiService.getApp()));
 }
 
 module.exports.registerApp = registerApp;
--- task/update.js
+++ task/update.js
@@ -1,4 +1,4 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 
-gulp.task('update', shell.task(['npm  update sharemyscreen-dev-service']));
+gulp.task('update', shell.task(['npm  update sharemyscreen-dev-service', 'npm  update sharemyscreen-login-service', 'npm  update sharemyscreen-api-service']));
