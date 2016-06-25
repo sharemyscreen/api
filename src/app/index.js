4cb749d005f1c97775f12840d8c3415344885023
Maxime Guedj <max.guedj13@gmail.com>
2016-06-25 02:53:16 +0200

    Integrate login app !

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,11 @@
 const vhost = require('vhost');
 const config = require('config');
 const devService = require('sharemyscreen-dev-service');
+const loginService = require('sharemyscreen-login-service');
 
 function registerApp (masterApp) {
   masterApp.use(vhost(config.get('devService.url'), devService.getApp()));
+  masterApp.use(vhost(config.get('loginService.url'), loginService.getApp()));
 }
 
 module.exports.registerApp = registerApp;
